fc2580b1f5a2e75accaa8363115bdebfd9dd88de
Alyssa Donawa <alyssadonawa@gmail.com>
2022-01-31 15:51:14 -0500

    minor clean up

--- src/AgnosticAnalyticsAdapter.js
+++ src/AgnosticAnalyticsAdapter.js
@@ -3,14 +3,6 @@ const fs = require('fs').promises;
 const Analytics = require('analytics');
 const AnalyticsAdapter = require('./AnalyticsAdapter');
 const Errors = require('./Errors');
-/* Initialize analytics */
-const analytics = Analytics({
-    app: '',
-    version: 100,
-    plugins: [
-    //  To-do.
-    ]
-  })
 
 /**
  * @class AgnosticAnalyticsAdapter
@@ -21,23 +13,24 @@ const analytics = Analytics({
      * Creates a new analytics adapter.
      * @param {Object} tracking id 
      */
-    constructor(tracking_id){
+    constructor(tracking_id, app_name, app_version, plugin_info){
         this.tracking_id = tracking_id;
-        // initialize analytics inside constructor, create a property on top of the constructor (const)
         // Look at Analytics original js code for how they construct
+        /* Initialize analytics */
+        const analytics = Analytics({
+            app: app_name,
+            version: app_version,
+            plugins: [
+            //  To-do.
+            ]
+        })
     }
     appOpened(parameters, req){
         return Promise.resolve({});
     }
     trackEvent(eventName, parameters, req){
         var visitor;
-        if (typeof(req.auth.user) !== 'undefined') {
-        // visitor = ua(this._tracking_id, req.auth.user.id);
         visitor = analytics.identify(this.tracking_id)
-        } else {
-        // visitor = ua(this._tracking_id);
-        visitor = analytics.identify(this.tracking_id)
-        }
         analytics.track(eventName, parameters)
         return Promise.resolve({});
     }
